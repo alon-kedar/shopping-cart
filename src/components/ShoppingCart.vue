f7b756c08201a6e5d449d5e2354af629bd29f6d8
Alon <alonkedar@gmail.com>
2018-07-14 18:57:06 +0300

    'modules'

---
+++ src/components/ShoppingCart.vue
@@ -0,0 +1,29 @@
+<script>
+  //---------
+  // Vue init
+  //---------
+  Vue.config.debug = false;
+  var vue = new Vue({
+    el: "#vue",
+
+
+  });
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
